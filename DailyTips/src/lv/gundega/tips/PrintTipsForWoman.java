6ca839196e117f513c27c4079f25cdf602861ec8
gundegaA <60237828+gundegaA@users.noreply.github.com>
2020-02-22 10:02:59 +0200

    22.02.

--- DailyTips/src/lv/gundega/tips/PrintTipsForWoman.java
+++ DailyTips/src/lv/gundega/tips/PrintTipsForWoman.java
@@ -1,13 +1,6 @@
 package lv.gundega.tips;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
 
